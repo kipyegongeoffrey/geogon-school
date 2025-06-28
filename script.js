9baef184f58780bfcb703f91646113db605b79fa
kipyegongeoffrey <136013930+kipyegongeoffrey@users.noreply.github.com>
2025-06-28 18:43:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('booking-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for your booking! We will contact you via email shortly.');
+});
